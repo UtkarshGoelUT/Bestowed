c766dc57a05efc2eade484215a69b8a1b0c32661
Arsh Kashyap <arshk102001@gmail.com>
2021-04-10 11:46:17 +0530

    added context

---
+++ src/context/userContext.js
@@ -0,0 +1,32 @@
+import React, { useState, createContext } from "react";
+
+export const userContext = createContext();
+
+export const UserProvider = (props) => {
+	const [uid, setUid] = useState("");
+	const [name, setName] = useState("");
+	const [amount, setAmount] = useState("");
+	const [transactions, setTransactions] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	return (
+		<userContext.Provider
+			value={{
+				uid,
+				name,
+				amount,
+				transactions,
+				loading,
+				setUid,
+				setAmount,
+				setName,
+				setTransactions,
+				setLoading,
+			}}
+		>
+			{props.children}
+		</userContext.Provider>
+	);
+};
+
+export default userContext;
